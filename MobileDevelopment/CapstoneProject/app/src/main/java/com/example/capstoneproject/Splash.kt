36e9adbcb3bedafc90ba41df296db8eb068d04b4
AzmiAbdillah <95601238+Levigodbot@users.noreply.github.com>
2023-06-16 22:40:19 +0700

    Add files via upload

---
+++ MobileDevelopment/CapstoneProject/app/src/main/java/com/example/capstoneproject/Splash.kt
@@ -0,0 +1,30 @@
+package com.example.capstoneproject
+
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowInsets
+import android.view.WindowManager
+
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash1)
+        @Suppress("DEPRECATION")
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        }else{
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        @Suppress("DEPRECATION")
+        Handler().postDelayed({
+            startActivity(Intent(this@Splash, Register::class.java))
+            finish()
+        },2500)
+    }
+}
